fe3b30b8132c4fb5b20ae0ac098b0a8f5e1ad06d
kazura233 <kazira233@gmail.com>
2023-11-18 21:18:51 +0800

    优化windowFission

--- packages/web-util/src/window-fission.ts
+++ packages/web-util/src/window-fission.ts
@@ -1,6 +1,6 @@
-import { win } from './add-event-listener'
+import { Win } from './add-event-listener'
 
-const KEY = 'WINDOW-FISSION'
+const KEY = '@kazura/web-util~window-fission__v1'
 
 /**
  * 每当打开一个新的页面(同浏览器同域)，便会执行一次listener。
@@ -9,7 +9,7 @@ const KEY = 'WINDOW-FISSION'
  */
 export function windowFission(listener: (event: StorageEvent) => any) {
   window.localStorage.setItem(KEY, Math.random().toString())
-  return win(window).addEventListener('storage', (event) => {
+  return Win.addEventListener('storage', (event) => {
     event.key === KEY && listener(event)
   })
 }

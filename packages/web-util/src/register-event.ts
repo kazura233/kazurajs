af6d275a988c41d454b360d091c803170a86ff05
kazura233 <kazira233@gmail.com>
2023-11-18 20:35:53 +0800

    Dom addEventListener

--- packages/web-util/src/register-event.ts
+++ packages/web-util/src/register-event.ts
@@ -1,3 +1,5 @@
+import { getElement } from './get-element'
+
 /**
  * 注册一个全局事件，并返回析构函数。
  * @param type
@@ -18,3 +20,28 @@ export function registerEvent(
   window.addEventListener(type, listener, options)
   return () => window.removeEventListener(type, listener, options)
 }
+
+export class Dom<T extends HTMLElement> {
+  readonly element: T
+
+  constructor(selector: T | string) {
+    this.element = getElement(selector)
+  }
+
+  addEventListener<K extends keyof WindowEventMap>(
+    type: K,
+    listener: (this: T, ev: WindowEventMap[K]) => any,
+    options?: boolean | AddEventListenerOptions
+  ): () => void
+  addEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | AddEventListenerOptions
+  ): () => void {
+    this.element.addEventListener(type, listener, options)
+
+    return () => this.element.removeEventListener(type, listener, options)
+  }
+}
+
+export const dom = <T extends HTMLElement>(selector: T | string) => new Dom(selector)

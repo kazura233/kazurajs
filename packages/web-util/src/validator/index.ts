2a918830d03b3ab2e261788fb742e3ec0c4e2ea3
kazura233 <kazira233@gmail.com>
2023-11-26 02:24:43 +0800

    优化validator

--- packages/web-util/src/validator/index.ts
+++ packages/web-util/src/validator/index.ts
@@ -6,3 +6,54 @@ export * from './object'
 export * from './string'
 export * from './typechecker'
 export * from './typed-is-promise'
+
+/**
+ * Object.prototype.toString
+ */
+export const objectToString = Object.prototype.toString
+
+/**
+ * Object.prototype.toString.call
+ *
+ * Object.prototype.toString.call(new Map()) // "[object Map]"
+ */
+export const toTypeString = (value: unknown): string => {
+  return objectToString.call(value)
+}
+
+/**
+ * 返回其原始类型的字符串表示
+ *
+ * Object.prototype.toString.call(new Map()).slice(8, -1) // "Map"
+ */
+export const toRawType = (value: unknown): string => {
+  return toTypeString(value).slice(8, -1)
+}
+
+/**
+ * 判断一个值是否为Map类型
+ */
+export const isMap = <K = any, V = any>(val: unknown): val is Map<K, V> =>
+  val instanceof Map && toTypeString(val) === '[object Map]'
+
+/**
+ * 判断一个值是否为Set类型
+ */
+export const isSet = <T = any>(val: unknown): val is Set<T> =>
+  val instanceof Set && toTypeString(val) === '[object Set]'
+
+/**
+ * 判断一个值是否为RegExp类型
+ */
+export const isRegExp = (val: unknown): val is RegExp =>
+  val instanceof RegExp && toTypeString(val) === '[object RegExp]'
+
+/**
+ * 判断一个值是否为函数类型
+ */
+export const isFunction = (val: unknown): val is Function => typeof val === 'function'
+
+/**
+ * 判断一个值是否为Symbol类型
+ */
+export const isSymbol = (val: unknown): val is symbol => typeof val === 'symbol'

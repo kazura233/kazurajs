d38ed27b95f24e755c96be1b78eeef2ed747ac75
kazura233 <kazira233@gmail.com>
2023-11-26 03:02:16 +0800

    @kazura/web-util formatDateStrSSS

--- packages/web-util/src/date/index.ts
+++ packages/web-util/src/date/index.ts
@@ -2,10 +2,18 @@ import dayjs from 'dayjs'
 
 /**
  * 格式化日期字符串
- * @param t - 日期字符串
+ * @param t - 日期字符串 (默认当前时间)
  * @returns 格式化后的日期字符串 ('YYYY-MM-DD HH:mm:ss')
  */
-export const formatDateStr = (t: string): string => {
-  // 使用 dayjs 解析日期字符串，然后格式化成指定格式
+export const formatDateStr = (t?: string): string => {
   return dayjs(t).format('YYYY-MM-DD HH:mm:ss')
+}
+
+/**
+ * 格式化日期字符串
+ * @param t - 日期字符串 (默认当前时间)
+ * @returns 格式化后的日期字符串 ('YYYY-MM-DD HH:mm:ss.SSS')
+ */
+export const formatDateStrSSS = (t?: string): string => {
+  return dayjs(t).format('YYYY-MM-DD HH:mm:ss.SSS')
 }

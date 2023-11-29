4eb62704c626e1a23ab2d9d371c4c9819ba5e7df
kazura233 <kazira233@gmail.com>
2023-11-29 11:20:06 +0800

    更新例子

--- packages/react-mobx/example/src/index.tsx
+++ packages/react-mobx/example/src/index.tsx
@@ -1,9 +1,9 @@
 import { Root } from '~/Root'
 import { createRoot } from 'react-dom/client'
-import { MobxProvider } from '@kazura/react-mobx'
+import { MobxProvider, createStores } from '@kazura/react-mobx'
 import { DemoStore } from './stores/demo-store'
 
-const stores = [DemoStore]
+const stores = createStores([DemoStore])
 
 const container = document.getElementById('root')!
 const root = createRoot(container)

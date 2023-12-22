991db2cd3cc1d0d5329f03c5f4c6aef897ccac56
kazura233 <kazura233@users.noreply.github.com>
2023-12-22 17:02:08 +0000

    deploy: 203a4c13fd2d063984e7e19bd55fdc7a1d140067

---
+++ assets/js/5e95c892.1fe849c7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[9661],{1374:(e,s,r)=>{r.r(s),r.d(s,{default:()=>l});r(79);var t=r(3230),u=r(6914),a=r(5838),c=r(6254),n=r(9071),i=r(5250);function l(e){return(0,i.jsx)(u.FG,{className:(0,t.Z)(a.k.wrapper.docsPages),children:(0,i.jsx)(n.Z,{children:(0,c.H)(e.route.routes)})})}}}]);

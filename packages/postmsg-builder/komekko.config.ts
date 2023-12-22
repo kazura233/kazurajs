98875cb447ac9980cc5c039181c3c397440faa65
kazura233 <kazira233@gmail.com>
2023-12-23 00:20:47 +0800

    @kazura/postmsg-builder Use komekko

---
+++ packages/postmsg-builder/komekko.config.ts
@@ -0,0 +1,32 @@
+import { type KomekkoOptions, defineConfig } from 'komekko'
+import pkg from './package.json'
+
+const fileName = pkg.name.split('/')[1]
+const name = 'PostMsgBuilder'
+const globals = {
+  '@kazura/web-postmsg': 'WebPostMsg',
+}
+
+const getOptions = (outFileName: string): KomekkoOptions => ({
+  outDir: './umd',
+  entries: [
+    {
+      input: './src/index.ts',
+      outFileName,
+      formats: ['umd'],
+    },
+  ],
+  rollupOptions: {
+    output: {
+      name,
+      globals,
+    },
+    external: [],
+  },
+})
+
+export default defineConfig([
+  {},
+  getOptions(fileName + '.js'),
+  { ...getOptions(fileName + '.min.js'), sourcemap: true, minify: true },
+])

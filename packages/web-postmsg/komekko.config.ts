f522865d9bba9c07aa3b2ca382b4fa6431aeed2d
kazura233 <kazira233@gmail.com>
2023-12-23 00:21:25 +0800

    @kazura/web-postmsg Use kemekko

---
+++ packages/web-postmsg/komekko.config.ts
@@ -0,0 +1,30 @@
+import { type KomekkoOptions, defineConfig } from 'komekko'
+import pkg from './package.json'
+
+const fileName = pkg.name.split('/')[1]
+const name = 'WebPostMsg'
+const globals = {}
+
+const getOptions = (outFileName: string): KomekkoOptions => ({
+  outDir: './umd',
+  entries: [
+    {
+      input: './src/index.ts',
+      outFileName,
+      formats: ['umd'],
+    },
+  ],
+  rollupOptions: {
+    output: {
+      name,
+      globals,
+    },
+    external: [],
+  },
+})
+
+export default defineConfig([
+  {},
+  getOptions(fileName + '.js'),
+  { ...getOptions(fileName + '.min.js'), sourcemap: true, minify: true },
+])

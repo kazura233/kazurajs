99b54601f794821824564f854269a7286547d4cc
kazura233 <kazira233@gmail.com>
2024-01-08 18:43:13 +0800

    web-util speak

--- packages/web-util/src/index.ts
+++ packages/web-util/src/index.ts
@@ -177,3 +177,14 @@ export const queueMicrotask: (callback: VoidFunction) => void =
   function (callback: VoidFunction) {
     Promise.resolve().then(callback)
   }
+
+/**
+ * 语音合成
+ * @param text
+ * @param lang
+ */
+export function speak(text: string, lang: string): void {
+  const speechInstance = new SpeechSynthesisUtterance(text)
+  speechInstance.lang = lang
+  speechSynthesis.speak(speechInstance)
+}

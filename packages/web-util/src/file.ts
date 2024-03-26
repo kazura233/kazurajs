99d546fa3fe42abf21870ad9974ca7f25175811b
kazura233 <kazira233@gmail.com>
2024-03-26 18:00:15 +0800

    DataURL，Base64，File，Blob，ArrayBufferView 互转

--- packages/web-util/src/file.ts
+++ packages/web-util/src/file.ts
@@ -1,3 +1,54 @@
+// ===== ===== ===== ===== ===== DataURL To ===== ===== ===== ===== ===== //
+
+/**
+ * DataURL 转 Uint8Array
+ * @param data - DataURL 字符串
+ * @returns - Uint8Array
+ */
+export const dataURLToArrayBufferView = (data: string): Uint8Array => {
+  return base64ToArrayBufferView(dataURLToBase64(data))
+}
+
+/**
+ * DataURL 转 Blob
+ * @param data - DataURL 字符串
+ * @returns - Blob
+ */
+export const dataURLToBlob = (data: string): Blob => {
+  const [head, base64] = data.split(',')
+
+  const match = head.match(/^data:(.*?);base64$/)
+
+  if (!match) {
+    throw new Error('Invalid DataURL format')
+  }
+
+  const [, mime] = match
+  return base64ToBlob(base64, mime)
+}
+
+/**
+ * DataURL 转 File
+ * @param data
+ * @param fileName
+ * @returns
+ */
+export const dataURLToFile = (data: string, fileName: string): File => {
+  return blobToFile(dataURLToBlob(data), fileName)
+}
+
+/**
+ * DataURL 转 Base64
+ * @param data
+ * @returns
+ */
+export const dataURLToBase64 = (data: string): string => {
+  const [, base64] = data.split(',')
+  return base64
+}
+
+// ===== ===== ===== ===== ===== Base64 To ===== ===== ===== ===== ===== //
+
 /**
  * Base64 转 Uint8Array
  * @param data - Base64 字符串
@@ -16,26 +67,6 @@ export const base64ToArrayBufferView = (data: string): Uint8Array => {
 }
 
 /**
- * DataURL 转 Uint8Array
- * @param data - DataURL 字符串
- * @returns - Uint8Array
- */
-export const dataURLToArrayBufferView = (data: string): Uint8Array => {
-  const [, base64] = data.split(',')
-  return base64ToArrayBufferView(base64)
-}
-
-/**
- * Uint8Array 转 Blob
- * @param u8arr - Uint8Array
- * @param mime - Blob 类型
- * @returns - Blob
- */
-export const arrayBufferViewToBlob = (u8arr: Uint8Array, mime: string): Blob => {
-  return new Blob([u8arr], { type: mime })
-}
-
-/**
  * Base64 转 Blob
  * @param data - Base64 字符串
  * @param mime - Blob 类型
@@ -46,19 +77,36 @@ export const base64ToBlob = (data: string, mime: string): Blob => {
 }
 
 /**
- * DataURL 转 Blob
- * @param data - DataURL 字符串
+ * Base64 转 File
+ * @param data
+ * @param fileName
+ * @param mime
+ * @returns
+ */
+export const base64ToFile = (data: string, fileName: string, mime: string): File => {
+  return arrayBufferViewToFile(base64ToArrayBufferView(data), fileName, mime)
+}
+
+/**
+ * Base64 转 DataURL
+ * @param data
+ * @param mime
+ * @returns
+ */
+export const base64ToDataURL = (data: string, mime: string): string => {
+  return `data:${mime};base64,${data}`
+}
+
+// ===== ===== ===== ===== ===== ArrayBufferView To ===== ===== ===== ===== ===== //
+
+/**
+ * Uint8Array 转 Blob
+ * @param u8arr - Uint8Array
+ * @param mime - Blob 类型
  * @returns - Blob
  */
-export const dataURLToBlob = (data: string): Blob => {
-  const match = data.match(/^data:(.*?);base64,(.*)$/)
-
-  if (!match) {
-    throw new Error('Invalid DataURL format')
-  }
-
-  const [, mime, base64] = match
-  return base64ToBlob(base64, mime)
+export const arrayBufferViewToBlob = (u8arr: Uint8Array, mime: string): Blob => {
+  return new Blob([u8arr], { type: mime })
 }
 
 /**
@@ -73,14 +121,17 @@ export const arrayBufferViewToFile = (u8arr: Uint8Array, fileName: string, mime:
 }
 
 /**
- * Blob 转 File
- * @param blob - Blob
- * @param fileName - 文件名
- * @returns - File
+ * Uint8Array 转 Base64
+ * @param u8arr
+ * @returns
  */
-export const blobToFile = (blob: Blob, fileName: string): File => {
-  return new File([blob], fileName, { type: blob.type })
+export const arrayBufferViewBase64 = (u8arr: Uint8Array): string => {
+  const buffer = u8arr.buffer
+  const base64String = btoa(String.fromCharCode(...new Uint8Array(buffer)))
+  return base64String
 }
+
+// ===== ===== ===== ===== ===== File To ===== ===== ===== ===== ===== //
 
 /**
  * File 转 Blob
@@ -89,6 +140,36 @@ export const blobToFile = (blob: Blob, fileName: string): File => {
  */
 export const fileToBlob = (file: File): Blob => {
   return new Blob([file], { type: file.type })
+}
+
+/**
+ * File 转 Base64
+ * @param file - File
+ * @returns - Promise 包装的 Base64 字符串
+ */
+export const fileToBase64 = (file: File): Promise<string> => {
+  return blobToBase64(fileToBlob(file))
+}
+
+/**
+ * File 转 Uint8Array
+ * @param file
+ * @returns
+ */
+export const fileToArrayBufferView = (file: File): Promise<Uint8Array> => {
+  return blobToArrayBufferView(fileToBlob(file))
+}
+
+// ===== ===== ===== ===== ===== Blob To ===== ===== ===== ===== ===== //
+
+/**
+ * Blob 转 File
+ * @param blob - Blob
+ * @param fileName - 文件名
+ * @returns - File
+ */
+export const blobToFile = (blob: Blob, fileName: string): File => {
+  return new File([blob], fileName, { type: blob.type })
 }
 
 /**
@@ -105,10 +186,28 @@ export const blobToBase64 = (blob: Blob): Promise<string> => {
 }
 
 /**
- * File 转 Base64
- * @param file - File
- * @returns - Promise 包装的 Base64 字符串
+ * Blob 转 Uint8Array
+ * @param blob
+ * @returns
  */
-export const fileToBase64 = (file: File): Promise<string> => {
-  return blobToBase64(fileToBlob(file))
+export const blobToArrayBufferView = (blob: Blob): Promise<Uint8Array> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+
+    reader.onload = function () {
+      const { result } = reader
+
+      if (result instanceof ArrayBuffer) {
+        resolve(new Uint8Array(result))
+      } else {
+        reject(new Error('Failed to read Blob as ArrayBuffer.'))
+      }
+    }
+
+    reader.onerror = function (event) {
+      reject(new Error('Failed to read Blob: ' + event.target?.error))
+    }
+
+    reader.readAsArrayBuffer(blob)
+  })
 }

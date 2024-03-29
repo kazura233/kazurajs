edde55fa2e1ddda4dc3216c04f251c34e369db25
kazura233 <kazira233@gmail.com>
2024-03-29 21:38:43 +0800

    @kazura/nestjs-redis 0.0.2

--- packages/nestjs-redis/src/index.ts
+++ packages/nestjs-redis/src/index.ts
@@ -1,3 +1,4 @@
-export * from './redis.service';
-export * from './redis.module';
-export * from './redis.interface';
+export * from './redis.constants'
+export * from './redis.service'
+export * from './redis.module'
+export * from './redis.interface'

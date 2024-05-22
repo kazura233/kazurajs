93eba87acd40ee8d062babf860855c081299eb87
kazura233 <kazira233@gmail.com>
2024-05-22 16:28:13 +0800

    @kazura/web-util 2.0.0

--- packages/web-util/src/validator/typed-is-promise.ts
+++
@@ -1,8 +0,0 @@
-// https://github.com/TylorS/typed-is-promise
-
-export function isPromise<T>(p: any): p is Promise<T>
-export function isPromise(p: any): p is Promise<any>
-
-export function isPromise<T>(p: any): p is Promise<T> {
-  return p !== null && typeof p === 'object' && typeof p.then === 'function'
-}

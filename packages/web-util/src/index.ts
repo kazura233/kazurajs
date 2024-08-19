aaff5503c0b1ff6c8e68ee991d742ee3bf846fd8
kazura233 <kazira233@gmail.com>
2024-08-19 19:59:21 +0800

    withResolvers

--- packages/web-util/src/index.ts
+++ packages/web-util/src/index.ts
@@ -203,3 +203,19 @@ export function assert(condition: boolean, msg: string) {
     throw new Error(msg)
   }
 }
+
+export function withResolvers<T>(): [
+  Promise<T>,
+  (value: T | PromiseLike<T>) => void,
+  (reason?: any) => void
+] {
+  let resolve: (value: T | PromiseLike<T>) => void
+  let reject: (reason?: any) => void
+
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res
+    reject = rej
+  })
+
+  return [promise, resolve!, reject!]
+}

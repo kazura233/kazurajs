7ff7d5b6b716aa83a6010fe3256574b83210d5a1
kazura233 <kazira233@gmail.com>
2024-12-15 00:37:32 +0800

    RouteObject

---
+++ packages/vite-plugin-react-pages/client-react.d.ts
@@ -0,0 +1,13 @@
+declare module '~react-pages' {
+  import type { RouteObject } from 'react-router'
+
+  const routes: RouteObject[]
+  export default routes
+}
+
+declare module 'virtual:generated-react-pages' {
+  import type { RouteObject } from 'react-router'
+
+  const routes: RouteObject[]
+  export default routes
+}

27a64d95cac6138c22a978960cc4b0ba4393ba63
kazura233 <kazira233@gmail.com>
2024-12-16 00:37:58 +0800

    Options

--- packages/vite-plugin-react-pages/src/types.ts
+++ packages/vite-plugin-react-pages/src/types.ts
@@ -1,5 +1,23 @@
 export interface Options {
+  /**
+   * 是否区分大小写
+   */
   caseSensitive?: boolean
+
+  /**
+   * 需要扫描的目录
+   */
+  dirs?: string[]
+
+  /**
+   * 需要忽略的目录
+   */
+  exclude: string[]
+
+  /**
+   * 需要扫描的文件扩展名
+   */
+  extensions: string[]
 }
 
 export interface ResolvedOptions extends Required<Options> {

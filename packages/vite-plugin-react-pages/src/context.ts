ea4aa5cc74c52085d3631482479d914f3e4dd639
kazura233 <kazira233@gmail.com>
2025-02-17 20:01:17 +0800

    vite-plugin-react-pages setupWatcher

--- packages/vite-plugin-react-pages/src/context.ts
+++ packages/vite-plugin-react-pages/src/context.ts
@@ -1,11 +1,32 @@
 import { ModuleNode, ViteDevServer } from 'vite'
 import { Options, ResolvedOptions } from './types'
-import { slash } from '@antfu/utils'
+import { slash, toArray } from '@antfu/utils'
 import { __debug_info, __debug_param } from './utils'
 import { VIRTUAL_MODULE_ID } from './constants'
+import { join, resolve } from 'node:path'
+import FastGlob from 'fast-glob'
+
+export interface PageRoute {
+  /**
+   * 文件路径
+   */
+  filePath: string
+
+  /**
+   * 原始路由
+   */
+  rawRoute: string
+
+  /**
+   * 是否为同步加载
+   */
+  sync: boolean
+}
 
 export class ReactPagesContext {
   private server: ViteDevServer | undefined
+
+  private routeMap = new Map<string, PageRoute>()
 
   private options: ResolvedOptions
 
@@ -17,10 +38,18 @@ export class ReactPagesContext {
   }
 
   resolveOptions(options: Options, root: string): ResolvedOptions {
-    const { caseSensitive = false } = options
+    const {
+      dirs = ['src/pages'],
+      exclude = ['internal', 'components'],
+      extensions = ['tsx', 'jsx', 'ts', 'js'],
+      caseSensitive = false,
+    } = options
 
     const resolvedOptions: ResolvedOptions = {
       root: slash(root),
+      dirs,
+      exclude,
+      extensions,
       caseSensitive,
     }
 
@@ -38,14 +67,53 @@ export class ReactPagesContext {
   setupWatcher(watcher: ViteDevServer['watcher']) {
     watcher.on('unlink', async (path) => {
       path = slash(path)
-      // TODO: remove page
+      if (!this.isPagesDir(path)) return
+      await this.removePage(path)
       this.onUpdate()
     })
     watcher.on('add', async (path) => {
       path = slash(path)
-      // TODO: add page
-      this.onUpdate()
+      if (!this.isPagesDir(path)) return
+      const routeMap = this.routeMap
+      await this.searchGlob()
+      if (JSON.stringify(routeMap) !== JSON.stringify(this.routeMap)) this.onUpdate()
     })
+  }
+
+  isPagesDir(path: string) {
+    for (const dir of this.options.dirs) {
+      const dirPath = slash(resolve(this.options.root, dir))
+      if (path.startsWith(dirPath)) return true
+    }
+    return false
+  }
+
+  async addPage(path: string | string[], pageDir: string) {
+    __debug_param('removePage->path', path)
+    __debug_param('removePage->pageDir', path)
+
+    for (const filePath of toArray(path)) {
+      const extension = this.options.extensions.find((ext) => filePath.endsWith(`.${ext}`))
+      if (!extension) continue
+
+      const pageDirPath = slash(resolve(this.options.root, pageDir))
+
+      let rawRoute = slash(filePath.replace(`${pageDirPath}/`, '').replace(`.${extension}`, ''))
+
+      const sync = filePath.endsWith('.sync')
+      if (sync) rawRoute = rawRoute.replace('.sync', '')
+
+      this.routeMap.set(filePath, {
+        filePath,
+        rawRoute,
+        sync,
+      })
+    }
+  }
+
+  async removePage(path: string) {
+    __debug_param('removePage->path', path)
+    this.routeMap.delete(path)
   }
 
   onUpdate() {
@@ -74,5 +142,43 @@ export class ReactPagesContext {
     }
   }
 
-  async searchGlob() {}
+  async searchGlob() {
+    const pageDirFiles = this.options.dirs.map((dir) => {
+      const pagesDirPath = slash(resolve(this.options.root, dir))
+      __debug_info('searchGlob->pagesDirPath', pagesDirPath)
+
+      const files = this.getPageFiles(pagesDirPath, this.options)
+      __debug_info('searchGlob->files', files)
+
+      return {
+        dir,
+        files: files.map((file) => slash(file)),
+      }
+    })
+
+    this.routeMap = new Map()
+    for (const page of pageDirFiles) {
+      await this.addPage(page.files, page.dir)
+    }
+
+    __debug_info('searchGlob->routeMap', this.routeMap)
+  }
+
+  private extsToGlob(extensions: string[]) {
+    return extensions.length > 1 ? `{${extensions.join(',')}}` : extensions[0] || ''
+  }
+
+  getPageFiles(path: string, options: ResolvedOptions): string[] {
+    const { exclude, extensions } = options
+
+    const ext = this.extsToGlob(extensions)
+
+    const files = FastGlob.sync(`**/*.${ext}`, {
+      ignore: exclude,
+      onlyFiles: true,
+      cwd: path,
+    }).map((p) => slash(join(path, p)))
+
+    return files
+  }
 }

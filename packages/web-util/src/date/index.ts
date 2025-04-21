e630bbcb7c2b40be3b6edc1f15a49caae0260480
kazura233 <kazira233@gmail.com>
2025-04-21 12:01:53 +0800

    @kazura/web-util 2.4.1

--- packages/web-util/src/date/index.ts
+++ packages/web-util/src/date/index.ts
@@ -4,7 +4,7 @@ import dayjs from 'dayjs'
 export { dayjs }
 
 export interface Formatter {
-  (date?: ConfigType): string | null
+  (date?: ConfigType): string
 }
 
 /**
@@ -16,96 +16,96 @@ export type ConfigType = dayjs.ConfigType
  * 格式化为 YYYY-MM-DD
  */
 export const formatDate: Formatter = (date) => {
-  return isNotEmpty(date) ? dayjs(date).format('YYYY-MM-DD') : null
+  return isNotEmpty(date) ? dayjs(date).format('YYYY-MM-DD') : ''
 }
 
 /**
  * 格式化为 YYYYMMDD
  */
 export const formatDateCompact: Formatter = (date) => {
-  return isNotEmpty(date) ? dayjs(date).format('YYYYMMDD') : null
+  return isNotEmpty(date) ? dayjs(date).format('YYYYMMDD') : ''
 }
 
 /**
  * 格式化为 HH:mm
  */
 export const formatTime: Formatter = (date) => {
-  return isNotEmpty(date) ? dayjs(date).format('HH:mm') : null
+  return isNotEmpty(date) ? dayjs(date).format('HH:mm') : ''
 }
 
 /**
  * 格式化为 YYYY-MM-DD HH:mm
  */
 export const formatDateTime: Formatter = (date) => {
-  return isNotEmpty(date) ? dayjs(date).format('YYYY-MM-DD HH:mm') : null
+  return isNotEmpty(date) ? dayjs(date).format('YYYY-MM-DD HH:mm') : ''
 }
 
 /**
  * 格式化为 YYYY-MM-DD HH:mm:ss
  */
 export const formatWithSeconds: Formatter = (date) => {
-  return isNotEmpty(date) ? dayjs(date).format('YYYY-MM-DD HH:mm:ss') : null
+  return isNotEmpty(date) ? dayjs(date).format('YYYY-MM-DD HH:mm:ss') : ''
 }
 
 /**
  * 格式化为 YYYY-MM-DD HH:mm:ss.SSS
  */
 export const formatWithMilliseconds: Formatter = (date) => {
-  return isNotEmpty(date) ? dayjs(date).format('YYYY-MM-DD HH:mm:ss.SSS') : null
+  return isNotEmpty(date) ? dayjs(date).format('YYYY-MM-DD HH:mm:ss.SSS') : ''
 }
 
 /**
  * 格式化为 YYYY-MM-DD 00:00:00
  */
 export const formatStartOfDay: Formatter = (date) => {
-  return isNotEmpty(date) ? dayjs(date).startOf('day').format('YYYY-MM-DD HH:mm:ss') : null
+  return isNotEmpty(date) ? dayjs(date).startOf('day').format('YYYY-MM-DD HH:mm:ss') : ''
 }
 
 /**
  * 格式化为 YYYY-MM-DD 23:59:59
  */
 export const formatEndOfDay: Formatter = (date) => {
-  return isNotEmpty(date) ? dayjs(date).endOf('day').format('YYYY-MM-DD HH:mm:ss') : null
+  return isNotEmpty(date) ? dayjs(date).endOf('day').format('YYYY-MM-DD HH:mm:ss') : ''
 }
 
 /**
  * 格式化为 YYYY-MM
  */
 export const formatYearMonth: Formatter = (date) => {
-  return isNotEmpty(date) ? dayjs(date).format('YYYY-MM') : null
+  return isNotEmpty(date) ? dayjs(date).format('YYYY-MM') : ''
 }
 
 /**
  * 格式化为 YYYYMM
  */
 export const formatYearMonthCompact: Formatter = (date) => {
-  return isNotEmpty(date) ? dayjs(date).format('YYYYMM') : null
+  return isNotEmpty(date) ? dayjs(date).format('YYYYMM') : ''
 }
 
 /**
  * 格式化为 YYYY
  */
 export const formatYear: Formatter = (date) => {
-  return isNotEmpty(date) ? dayjs(date).format('YYYY') : null
+  return isNotEmpty(date) ? dayjs(date).format('YYYY') : ''
 }
 
 /**
  * 格式化为 MM-DD
  */
 export const formatMonthDay: Formatter = (date) => {
-  return isNotEmpty(date) ? dayjs(date).format('MM-DD') : null
+  return isNotEmpty(date) ? dayjs(date).format('MM-DD') : ''
 }
 
 /**
  * 格式化为 HH:mm:ss
  */
 export const formatTimeWithSeconds: Formatter = (date) => {
-  return isNotEmpty(date) ? dayjs(date).format('HH:mm:ss') : null
+  return isNotEmpty(date) ? dayjs(date).format('HH:mm:ss') : ''
 }
 
 /**
  * 格式化为 HH:mm:ss.SSS
  */
 export const formatTimeWithMilliseconds: Formatter = (date) => {
-  return isNotEmpty(date) ? dayjs(date).format('HH:mm:ss.SSS') : null
+  return isNotEmpty(date) ? dayjs(date).format('HH:mm:ss.SSS') : ''
 }

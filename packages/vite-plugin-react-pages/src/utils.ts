dbf7c9d155e73db6996698e78905f1082727c85a
kazura233 <kazira233@gmail.com>
2026-07-10 14:19:22 +0800

    移除 vite-plugin-react-pages

--- packages/vite-plugin-react-pages/src/utils.ts
+++
@@ -1,5 +0,0 @@
-import debug from 'debug'
-import { PLUGIN_NAME } from './constants'
-
-export const __debug_param = debug(`${PLUGIN_NAME}:param`)
-export const __debug_info = debug(`${PLUGIN_NAME}:info`)
